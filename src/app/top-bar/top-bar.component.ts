99ddd1c4383a1c89e7774e2f2ece420a0c6300c0
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-02-26 21:23:44 +0100

    first update

---
+++ src/app/top-bar/top-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.scss'
+})
+export class TopBarComponent {
+  menus = ['Accueil', 'Projets', 'Contact'];
+
+}
