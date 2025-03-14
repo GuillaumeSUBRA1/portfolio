9b6e2dac23dfdf5cfa6864ba0f3c04045c760324
GuillaumeSUBRA1 <74560838+GuillaumeSUBRA1@users.noreply.github.com>
2025-03-14 11:10:18 +0100

    add services

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
+import { FieldsetModule } from 'primeng/fieldset';
 
 @Component({
   selector: 'services',
   standalone: true,
-  imports: [],
+  imports: [FieldsetModule],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
 
+  services = [
+    {
+      titre: "Développement Web",
+      description: "Création et maintenance de sites web interactifs et performants.",
+      icon: "web"
+    },
+    {
+      titre: "Développement Mobile",
+      description: "Création et maintenance d'applications mobiles interactives et performantes.",
+      icon: "web"
+    },
+    {
+      titre: "Création portfolio",
+      description: "Création d'un portfolio personnalisé.",
+      icon: "portfolio"
+    },
+    {
+      titre: "Modèle de données",
+      description: "Création de modèles de données pour les applications.",
+      icon: "database"
+    }
+  ]
 }
